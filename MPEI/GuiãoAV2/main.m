7ebe3812ca0b49a0344d3f01c3359d75692c8af9
Pjnp5 <77510846+Pjnp5@users.noreply.github.com>
2022-01-28 16:13:17 +0000

    Update main.m

--- MPEI/GuiãoAV2/main.m
+++ MPEI/GuiãoAV2/main.m
@@ -1,48 +1,36 @@
 clc;
 clear all;
 
-load utilizadores.txt
-; %'dic' 'Nu' 'users' 'Set' 'MinHashValue' 'MinHashSig'
-users
-user = 0;
-option = 0;
+utilizadores = fopen("utilizadores.txt");
 
-menu(user, option, Nu, Set, dic, MinHashValue, MinHashSig);
-
-function menu(utilizador, opcao, Nu, Set, dic, MinHashValue, MinHashSig)
-    while(isempty(option) || option < 4 || user == 0)
-        clc
-        if (user == 0)
-            user = str2num(input(['Insert User ID (1 to ' num2str(Nu) '): '], 's'));
-        elseif (isempty(user) || user< 1 || user>Nu)
-            fprintf(2, 'User ID not valid.');
-            fprintf(2, '\nPress any key to continue. ');
-            pause; clc;  % Manter a informação disponível até ao utilizador pressionar em qualquer tecla
-            user = 0;
-        else
-            fprintf('\nUser ID: %d\nMenu', utilizador);
-            fprintf('\n1 - Your Movies\n2 - Get Suggestions\n3 - Search Title\n4 - Exit\nSelect choice: ');
-            option = str2num(input('', 's'));
-            if isempty(option)
-                continue;
-            end
-            
-            switch option
+while(isempty(option) || option < 4 || user == 0)
+    clc
+    if (user == 0)
+        user = str2num(input(['Insert User ID (1 to ' num2str(Nu) '): '], 's'));        
+    elseif (isempty(user) || user< 1 || user>Nu)
+        fprintf(2, 'User ID not valid.');
+        fprintf(2, '\nPress any key to continue. ');
+        pause; clc;  % Manter a informação disponível até ao utilizador pressionar em qualquer tecla
+        user = 0;
+    else
+        fprintf('\nUser ID: %d\nMenu', utilizador);
+        fprintf('\n1 - Your Friends\n2 - Interests from most similar user\n3 - Search Name\n4 - Find most similar users based in list of interests \n5 - Exit\nSelect choice: ');
+        option = str2num(input('', 's'));
+        if isempty(option)
+            continue;
+        end    
+        switch option
                 case 1
-                    YourFriends(user, Set, dic);
+                    YourFriends();
                 case 2
-                    Interests(Nu, MinHashValue, user, Set, dic);
+                    Interests();
                 case 3
-                    searchName(dic, MinHashSig)
+                    searchName()
                 case 4
                     FindMostSimilarUser()
                 case 5
                     clc
-                    break;
-                otherwise
-                    option = 0;
-                    clc
-            end
-        end
+                    break
+         end
     end
 end
